2356bafd6eac943d43521ce562c7b3d14dc83e71
yulunnie <nie.yu@huskey.neu.edu>
2021-04-08 23:29:37 -0700

    add a max filter to run a simplified version

--- src/main/scala/UserRank.scala
+++ src/main/scala/UserRank.scala
@@ -19,47 +19,52 @@ object UserRank {
     val sc = new SparkContext(conf)
 
     // parameter setting
-    val dummy = 0         // a dummy to store leak values due to dangling nodes
+    val dummy = -2        // a dummy to store leak values due to dangling nodes
     val root = -1         // a root node connect to all front node
-    val k = 10            // max node id to process, test use
+    val max = 100        // max node id to process, test use
     val initialRank = 1f  // initial UserRank
     val alpha = 0.15      // alpha value for random jump
-    val iterations = 2    // total iteration times
+    val iterations = 1    // total iteration times
 
     // create (dangling, dummy)
     val danglingToDummy = sc.textFile("input/danglings.csv")
+      .filter( id => id.toInt < max )
       .map( id => (id.toInt, dummy) )
+
     // create (root, front)
     val rootToFront = sc.textFile("input/fronts.csv")
+      .filter( id => id.toInt < max )
       .map( id => (root, id.toInt) )
 
     // load edges
     val edges = sc.textFile("input/musae_git_edges.csv")
       .map( line => line.split(","))
       .map( fromTo => (fromTo(0).toInt, fromTo(1).toInt) )
+      .filter{ case (from, to) => from < max && to < max }
 
-    val graph = sc.parallelize(edges, 20).partitionBy(new HashPartitioner(100))
+    // create the complete graph
+    val graph = edges.union(danglingToDummy).union(rootToFront)
 
-    // create page rank table
-    var pageRanks : List[(Int,Float)] = List()
-    // page rank of dummy and root is all 0
-    pageRanks = pageRanks :+ (dummy, 0f)
-    pageRanks = pageRanks :+ (root, 0f)
-    for (i <- 1 to k*k) {
-      // page rank of all real node is 1/k*k
-      pageRanks = pageRanks :+ (i, initialRank)
+    // create rank table
+    var ranksList : List[(Int,Float)] = List()
+    // rank of dummy and root is all 0
+    ranksList = ranksList :+ (dummy, 0f)
+    ranksList = ranksList :+ (root, 0f)
+    for (i <- 0 to max) {
+      // rank of all real node is 1
+      ranksList = ranksList :+ (i, initialRank)
     }
-    var ranks = sc.parallelize(pageRanks, 20).partitionBy(new HashPartitioner(100))
+    var ranks = sc.parallelize(ranksList, 20).partitionBy(new HashPartitioner(100))
 
     // iteration
     for (i <- 0 until iterations) {
-      // generate a (pageId, pageRank) table for all nodes
+      // generate a (id, rankValue) table for all nodes
       val contributions = graph.groupByKey().join(ranks).flatMap{
-        case (pageId, (links, rank)) => links.map(dest => (dest, rank / links.size))
+        case (id, (links, rank)) => links.map(dest => (dest, rank / links.size))
       }
       // compute the leak/#node
-      val distribution = contributions.lookup(0).sum / (k*k)
-      ranks = contributions.reduceByKey(_ + _).mapValues(v => (alpha/(k*k) + (1f-alpha)*(v + distribution)).toFloat)
+      val distribution = contributions.lookup(0).sum / max
+      ranks = contributions.reduceByKey(_ + _).mapValues(v => (alpha/max + (1f-alpha)*(v + distribution)).toFloat)
       ranks = ranks.union(sc.parallelize(List((root, 0f))))
     }
 
